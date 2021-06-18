d04840ec246dbfc686fbb8b9a4f5adaa782df5d1
Bartek Mielczarek <drahek@gmail.com>
2021-06-18 14:08:06 +0200

    added alternative version of js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let formElement = document.querySelector(".js-form");
+/* let formElement = document.querySelector(".js-form");
 let amountElement = document.querySelector(".js-amount");
 let currencyElement = document.querySelector(".js-currency");
 let resultElement = document.querySelector(".js-result");
@@ -31,3 +31,49 @@ formElement.addEventListener("submit", (event) => {
 
     resultElement.innerHTML = `Otrzymasz <strong>${result.toFixed(2)} ${currency}</strong>`;
 })
+*/
+ 
+{
+    const calculateResult = (amount, currency) => {
+        const rateEUR = 4.5654;
+        const rateGBP = 5.2491;
+        const rateUSD = 3.9058;
+
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
+
+            case "GBP":
+                return amount / rateGBP;
+
+            case "USD":
+                return amount / rateUSD;
+        }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `Otrzymasz <strong>${result.toFixed(2)} ${currency}</strong>`;
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+    };
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
